3d5c14113220d312966f37e411d463b1b0d1f47c
CJlano <cjlano+github@free.fr>
2013-03-07 22:08:59 +0100

    Bezier curve with N definition points
    + draw construction lines in RED
    + draw bezier curve in BLUE

--- bezier.py
+++ bezier.py
@@ -1,22 +1,36 @@
-import Image
-
-# im = Image.new("1", (100,100), "white")
-# im.putpixel((10,10),1)
-# im.putpixel((20,10),1)
-# im.putpixel((30,30),1)
-# 
-# im.show()
+import os
+import Image, ImageDraw
 
 def bezier1(p0, p1, t):
-	x = p0[0] + t * (p1[0] - p0[0])
-	y = p0[1] + t * (p1[1] - p0[1])
-	return (x,y)
+    x = p0[0] + t * (p1[0] - p0[0])
+    y = p0[1] + t * (p1[1] - p0[1])
+    return (x,y)
 
-im = Image.new("1", (100,100), "white")
+def bezierN(pts, t):
+    res = list(pts)
+    for n in range(len(pts), 1, -1):
+        for i in range(0,n-1):
+            res[i] = bezier1(res[i], res[i+1], t)
+    return res[0]
 
+im = Image.new("RGB", (100,100), "white")
+draw = ImageDraw.Draw(im)
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+pts = [(10,90),(5,40),(60,40),(90,90)]
+for pt in pts:
+    im.putpixel(pt, red)
+draw.line(pts,fill=red)
+
+bezier = []
 for t in range(0,10):
-	xy = bezier1((0,0), (10,10), t*0.1)
-	im.putpixel((int(round(xy[0])), int(round(xy[1]))), 1)
-#	im.putpixel(xy,1)
+    xy = bezierN(pts, t*0.1)
+    bezier.append(xy)
+    im.putpixel((int(round(xy[0])), int(round(xy[1]))), green)
 
-im.show()
+draw.line(bezier, fill=blue)
+im.save(os.path.expanduser("~/public_html/bezier.png"))
+#im.show()
