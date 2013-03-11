ed2dcbb53ecde004b3f316ce6191cb951f68f783
CJlano <cjlano+github@free.fr>
2013-03-11 22:12:38 +0100

    prepare dict{} usage for command parsing

--- str.py
+++ str.py
@@ -13,14 +13,24 @@ path_str = sys.argv[1]
 path = re.split(r"([+-]?\ *\d+(?:\.\d*)?|\.\d+)", path_str)
 # (?:...)non-capturing version of regular parentheses
 # because re.split()  If capturing parentheses are used in pattern, then the text of all groups in the pattern are also returned as part of the resulting list
-print len(path)
+
+# Number of points per commands
+cmd = {'M':1, 'C':3, 'Z':0, 'L':1}
 
 for i,elt in enumerate(path):
+# Remove whitespaces
     elt = elt.replace(' ','')
+# Remove empty elements
     if elt == '':
         path.pop(i)
-    if elt.isalpha():
-        print elt
+# Separate multiple elements (e.g.: 'ZM')
+    if elt.isalpha() and len(elt) > 1:
+        print elt[0]
+# Search in cmd
+#    if elt.upper() in cmd.keys():
+#        print cmd[elt.upper()]
+
+# Change lower case command to upper case (relative to absolute)
 
 print len(path)
 
