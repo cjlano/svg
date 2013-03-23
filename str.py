140d21a1c47f909ab3242f76988a8ad184238cf2
CJlano <cjlano+github@free.fr>
2013-03-23 11:46:28 +0100

    Parse regex result list
     - Remove all spaces
     - Remove ','
     - Check command validity
     - convert numeric string to float()

--- str.py
+++ str.py
@@ -6,52 +6,36 @@ if len(sys.argv) < 2:
     print "Usage: %s \"path string\"" % sys.argv[0]
     sys.exit()
 
-path_str = sys.argv[1]
-
-#print path
-
-path = re.split(r"([+-]?\ *\d+(?:\.\d*)?|\.\d+)", path_str)
+path = re.split(r"([+-]?\ *\d+(?:\.\d*)?|\.\d+)", sys.argv[1])
 # (?:...)non-capturing version of regular parentheses
 # because re.split()  If capturing parentheses are used in pattern, then the text of all groups in the pattern are also returned as part of the resulting list
 
-#clean-up path in p[]
+# Number of expected values per commands
+cmd = {'M':2, 'L':2, 'Z':0, 'H':1, 'V':1, 'A':7, 'Q':4, 'T':2, 'C':6, 'S':4}
+
+# clean-up path in p[]
 p = []
 for elt in path:
+# remove all spaces
     elt = elt.strip()
     elt = elt.replace(' ','')
     if (elt == ''):
         continue;
-    if elt.isalpha() and  len(elt) > 1:
+# remove all commas (not strictly necessary in SVG)
+    if (elt == ','):
+        continue;
+# split commands into single one (e.g.: 'ZM' -> 'Z','M')
+# check command validity
+    if elt.isalpha():
         for i in list(elt):
-            p.append(i)
+            if (i.upper() in cmd.keys()): p.append(i)
+            else: print i, " is not a valid command"
+# not a command? should be a numeric
     else:
-        p.append(elt)
+        try: p.append(float(elt))
+        except: print elt, " should be numeric"
 
 print p
-sys.exit(0)
-    
-
-# Number of points per commands
-cmd = {'M':1, 'C':3, 'Z':0, 'L':1}
-
-for i,elt in enumerate(path):
-# Remove whitespaces
-    #elt = elt.replace(' ','')
-    elt = elt.strip()
-# Remove empty elements
-    if elt == '':
-        path.pop(i)
-# Separate multiple elements (e.g.: 'ZM')
-    if elt.isalpha() and len(elt) > 1:
-        print elt[0]
-	path.insert(i+1, elt[1])
-	elt = elt[0]
-# Search in cmd
-#    if elt.upper() in cmd.keys():
-#        print cmd[elt.upper()]
 
 # Change lower case command to upper case (relative to absolute)
 
-print len(path)
-
-print path
