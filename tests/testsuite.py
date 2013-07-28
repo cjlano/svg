3904c419d3526d7a88dac196601bb75a3b8fe040
CJlano <cjlano+github@free.fr>
2013-07-28 21:58:27 +0200

    Tests: only test .svg files

--- tests/testsuite.py
+++ tests/testsuite.py
@@ -7,7 +7,8 @@ path = 'W3C_SVG_11_TestSuite/svg/'
 
 def test_files():
     for filename in os.listdir(path):
-        yield svg.Svg, path+filename
+        if filename[-3:] == 'svg':
+            yield svg.Svg, path+filename
 
 if __name__=="__main__":
     import nose
