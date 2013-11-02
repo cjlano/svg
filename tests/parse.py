80026af3dc22d0e0c290f8bd41d3798b41989ed4
CJlano <cjlano+github@free.fr>
2013-11-02 21:34:52 +0100

    Tests: parse.py try to parse every W3C testsuite file
    Main purpose is to collect messages from parser (e.g.: No handler for
    <tag> ... )

---
+++ tests/parse.py
@@ -0,0 +1,16 @@
+# Parse all W3C SVG testsuite files
+# use it with 
+## python parse.py | grep ^"No handler" | sort | uniq -c | sort -n
+# to get all unhandled elements sorted by occurence.
+
+import os
+import sys
+sys.path.append('..') #FIXME
+import svg
+
+path = 'W3C_SVG_11_TestSuite/svg/'
+
+for f in os.listdir(path):
+    if os.path.splitext(f)[1] == '.svg':
+        svg.Svg(path + f)
+
